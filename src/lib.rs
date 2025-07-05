90243f7902f3bfe3376930866a9643ee5622b3b1
Nik Revenco <pm@nikrev.com>
2025-07-05 20:53:15 +0100

    chore: rename `comment_line` to `comment`

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
+//! This crate contains the [`comment`] function, which turns the passed input
+//! into a comment for the given file type
+
 use std::path::Path;
 
 mod generated;
 
-pub fn comment_line<S: ToString, P: AsRef<Path>>(content: S, path: P) -> String {
-    generated::comment_line(content, path)
+/// Turn the given `content` into a comment for the given `path`
+///
+/// # Examples
+///
+/// ```
+/// use comment_line::comment;
+///
+/// assert_eq!(comment("hello, world!", "index.html"), "<!-- hello, world! -->");
+/// assert_eq!(comment("hello, world!", "main.rs"), "/* hello, world! */");
+/// assert_eq!(comment("hello, world!", "script.lua"), "--[[ hello, world! --]]");
+/// assert_eq!(comment("hello, world!", "123456789"), "# hello, world!");
+/// ```
+pub fn comment<S: ToString, P: AsRef<Path>>(content: S, path: P) -> String {
+    generated::comment(content, path)
 }
