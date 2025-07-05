1f7263119e0f49b324bf30378b3eb757dc181e27
Nik Revenco <pm@nikrev.com>
2025-07-05 20:18:09 +0100

    feat: implement the basic functionality of the library

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
-//! lib docs
+use std::path::Path;
+
+mod generated;
+
+pub fn comment_line<S: ToString, P: AsRef<Path>>(content: S, path: P) -> String {
+    generated::comment_line(content, path)
+}
