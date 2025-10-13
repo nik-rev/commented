64c67fd0c3bc9cc6d1077ff73d5e05733baf7176
Nik Revenco <pm@nikrev.com>
2025-10-13 18:12:35 +0100

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
 //! This crate contains the [`comment`] function, which turns the passed input
 //! into a comment for the given file type
 //!
+//! ```toml
+//! commented = "0.1.0"
+//! ```
+//!
+//! # Usage
+//!
+//! ```rust
+//! use commented::comment;
+//!
+//! assert_eq!(comment("hello, world!", "index.html"), "<!-- hello, world! -->");
+//! assert_eq!(comment("hello, world!", "main.rs"), "/* hello, world! */");
+//! assert_eq!(comment("hello, world!", "script.lua"), "--[[ hello, world! --]]");
+//! // fallback to `#`, as that's the most common comment tokens for
+//! // files without an extension
+//! assert_eq!(comment("hello, world!", "123456789"), "# hello, world!");//!
+//! ```
+//!
+//! # Features
+//!
 //! Enabling the `glob` feature will *slightly* improve accuracy of the comment heuristics.
 
 use std::path::Path;
